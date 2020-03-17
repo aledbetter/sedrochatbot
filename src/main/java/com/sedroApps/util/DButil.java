535599140c4acee21f5fa7b0579455ed7af25134
aaronledbetter <aaronledbetter@192.168.1.5>
2020-03-17 06:44:32 -0700

    packaging, deployment and maven run

--- src/main/java/com/sedroApps/util/DButil.java
+++ src/main/java/com/sedroApps/util/DButil.java
@@ -40,7 +40,8 @@ public class DButil {
 	
     static String prod_user = null;
     static String prod_pass = null;
-    static String prod_DB = "jdbc:postgresql://aaip4f08ft38a.cg07na9ichnw.us-east-2.rds.amazonaws.com:5432/postgres";
+    static String prod_DB = "jdbc:postgresql://aafbtx2xad9u71.cg07na9ichnw.us-east-2.rds.amazonaws.com:5432/ebdb";
+    
     static String prod_db = "public";
 	static final String JDBC_DRIVER = "org.postgresql.Driver";  //org.postgresql.Driver
 	static boolean dbinit = false;
